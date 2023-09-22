40b75801a47754bc4335421d16f5e937826c25cd
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-09-22 08:41:05 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Adriano Miguel dos Santos!");
+        System.out.println("today, September 22, 2023, I begin my studies on the Java programming language.");
+    }
+}
